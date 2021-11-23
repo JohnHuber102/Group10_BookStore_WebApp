f298a33d30e569d885f5fc1efb93a4bad944d776
JohnHuber102 <74277417+JohnHuber102@users.noreply.github.com>
2021-11-22 19:46:41 -0500

    Update User.java

--- src/main/java/com/Group10/bookstore/User.java
+++ src/main/java/com/Group10/bookstore/User.java
@@ -1,23 +1,100 @@
 package com.Group10.bookstore.Users;
 
-import org.springframework.web.bind.annotation.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 
-@RestController
-public class LoginController {
-    public User user;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-    @RequestMapping(value = "/login")
-    public String login(){
-        return "Login Screen";
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+public class User extends LoginController implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
+  public void UserDetails(User user) {
+       this.user = user;
+    }
+
+@Id
+public Integer id;
+
+public String username;
+
+public String password;
+
+public Integer getId(){
+    return id;
+}
+
+public void setId(Integer id){
+    this.id = id;
+}
+
+public String getUsername(){
+    return username;
+}
+
+public void setUsername(String username){
+    this.username = username;
+}
+
+public String getPassword(){
+    return password;
+}
+
+public void setPassword(String password){
+    this.password = password;
+}
+
+@Override
+public Set<GrantedAuthority> getAuthorities(){
+    Set<GrantedAuthority> authorities = new HashSet<>();
+    authorities.add(new SimpleGrantedAuthority("USER"));
+    return authorities;
+}
+
+   public User getUser() {
+        return user;
+    }
+
+    public boolean isAccountNonExpired() {
+        return true;
     }
 
 
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-
-    public String login(@RequestBody User user) {
-        user = user;
-        return "Success";
+    public boolean isAccountNonLocked() {
+        return true;
     }
+
+
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public String getFullName() {
+        return user.getFirstName() + " " + user.getLastName();
+    }
+
+
+    public String getFirstName(){
+       return getFirstName();
+    }
+
+  public String getLastName(){
+
+        return getLastName();
+  }
 
 }
